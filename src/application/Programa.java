2e9f3a932294599aae449144fe70d12d34ae10f8
DarknessHunterr <lucarbass@live.com>
2019-04-07 17:00:40 -0300

    Classe entity Vendedor

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,11 +1,17 @@
 package application;
 
 import model.entities.Departamento;
+import model.entities.Vendedor;
+
+import java.util.Date;
 
 public class Programa {
     public static void main(String[] args) {
 
         Departamento obj = new Departamento(1,"Livros");
-        System.out.println(obj);
+
+        Vendedor vendedor = new Vendedor(21,"Bob","bob@gmail.com", new Date(), 3000.00,obj);
+
+        System.out.println(vendedor);
     }
 }
